67e42900a98cd686919dd6e4cff7f3670fcdc76c
ms <mateusz@>
2023-07-13 13:52:24 +0200

    create render and two simple function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    accept: false
+  }
+  handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name] : value
+  })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  render() {
+    const {username,email,password,accept} = this.state 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onChange={this.handleSubmit}>
+        <label>
+          Twoje imię: <input htmlFor="user" id='user' name="username" value={username} type='text' onChange={this.handleChange} ></input>
+        </label>
+        <label>
+          Twój email: <input htmlFor="email" id='email' name={email} type='email' onChange={this.handleChange}></input>
+        </label>
+        <label>
+          Twoje hasło: <input htmlFor="password" id='password' name={password} type='password' onChange={this.handleChange}></input>
+        </label>
+        <label><input htmlFor="accept" id='accept' type='checkbox' name={accept} onChange={this.handleChange}></input>Zapoznałem się z regulaminem.
+        <br/>
+        By się zapisać potwierdź akceptację regulaminu.
+        </label>
+        <button>Wyślij formularz</button>
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
