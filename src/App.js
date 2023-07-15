b9ad4550c40c0b9591f175d383de6ce528166c3a
ms <mateusz@>
2023-07-15 10:20:33 +0200

    create messages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ class App extends Component {
     password: '',
     accept: false,
   }
+
+  message = {
+    badUsername : 'Nazwa musi mieć co najmniej 10 liter i nie może zawierać spacji',
+    badEmail : 'Brak @, podaj prawidłowy email',
+    badPassword: 'Zła liczba znaków, hasło musi mieć 10 znaków',
+    badAccept : 'Zaznacz potwierdzenie regulaminu'
+
+  }
+
   handleChange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
@@ -25,13 +34,16 @@ class App extends Component {
     <div className="App">
       <form onChange={this.handleSubmit}>
         <label>
-          Twoje imię: <input htmlFor="user" id='user' name="username" value={username} type='text' onChange={this.handleChange} ></input>
-        </label>
+          Twoje imię: <input htmlFor="user" id='user' name="username" value={username} type='text' onChange={this.handleChange}  ></input>
+          {this.message.badUsername}
+        </label> 
         <label>
           Twój email: <input htmlFor="email" id='email' name={email} type='email' onChange={this.handleChange}></input>
+          {this.message.badEmail}
         </label>
         <label>
           Twoje hasło: <input htmlFor="password" id='password' name={password} type='password' onChange={this.handleChange}></input>
+          {this.message.badPassword}
         </label>
         <label><input htmlFor="accept" id='accept' type='checkbox' name={accept} onChange={this.handleChange}></input>Zapoznałem się z regulaminem.
         <br/>
